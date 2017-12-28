bdb1b7f5eda90f1fce43e38b59b83162447f12bd
Pawel <pasekpasek@gmail.com>
2017-12-28 14:17:25 +0100

    Spring java config

--- spring-boot-jokes-app/src/main/java/guru/springframework/springbootjokesapp/services/impl/DefaultChuckNorrisJokeService.java
+++ spring-boot-jokes-app/src/main/java/guru/springframework/springbootjokesapp/services/impl/DefaultChuckNorrisJokeService.java
@@ -2,12 +2,18 @@ package guru.springframework.springbootjokesapp.services.impl;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import guru.springframework.springbootjokesapp.services.ChuckNorrisJokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DefaultChuckNorrisJokeService implements ChuckNorrisJokeService {
 
-    ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
+    ChuckNorrisQuotes quotes;
+
+    @Autowired
+    public DefaultChuckNorrisJokeService(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
+    }
 
     @Override
     public String getRandomJoke() {
