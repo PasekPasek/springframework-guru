9dde96cc957c656c65f625123b41aaff8bc31da9
Pawel <pasekpasek@gmail.com>
2018-02-02 22:50:26 +0100

    One to Many relations

---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,49 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String desscription;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private  Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesscription() {
+        return desscription;
+    }
+
+    public void setDesscription(String desscription) {
+        this.desscription = desscription;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
