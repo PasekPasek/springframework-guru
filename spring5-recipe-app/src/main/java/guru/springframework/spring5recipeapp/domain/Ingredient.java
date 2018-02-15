164a8e3cd6bdd8f6e4709016de62c411d5e405cd
Pawel <pasekpasek@gmail.com>
2018-02-15 23:23:47 +0100

    Create command classess and converters

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -13,7 +13,7 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String desscription;
+    private String description;
     private BigDecimal amount;
 
     @OneToOne(fetch = FetchType.EAGER)
@@ -25,14 +25,14 @@ public class Ingredient {
     public Ingredient() {
     }
 
-    public Ingredient(String desscription, BigDecimal amount, UnitOfMeasure uom) {
-        this.desscription = desscription;
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
         this.amount = amount;
         this.uom = uom;
     }
 
-    public Ingredient(String desscription, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.desscription = desscription;
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
         this.amount = amount;
         this.uom = uom;
         this.recipe = recipe;
--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -42,14 +42,16 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Recipe addIngredient(Ingredient ingredient){
+    public Recipe addIngredient(Ingredient ingredient) {
         ingredient.setRecipe(this);
         this.ingredients.add(ingredient);
         return this;
     }
 
     public void setNotes(Notes notes) {
-        this.notes = notes;
-        notes.setRecipe(this);
+        if (notes != null) {
+            this.notes = notes;
+            notes.setRecipe(this);
+        }
     }
 }
